33ac80a92b05ca257c2c3a5038824ae8d5957e80
msharma-tw <monika.sharma@thoughtworks.com>
2021-03-09 13:24:58 +0530

    Initial Setup | Monika

---
+++ test/upload_document_test.js
@@ -0,0 +1,26 @@
+const request = require("supertest")("http://localhost:3003");
+const expect = require("chai").expect;
+const fs = require('fs');
+const path = require('path');
+
+const document = path.resolve('./zipFile.zip');
+const config = {
+    'formData': {
+        'file': fs.createReadStream(document),
+    },
+    'headers': {
+        'Content-Type': 'multipart/form-data'
+    }
+}
+
+describe("POST /document/upload", function() {
+    it("should upload doc to user profile", async function () {
+
+        const uploadDocResponse = await request
+            .post("/document/upload")
+            .send(config);
+
+        expect(uploadDocResponse.status).to.eql(200);
+        console.log(uploadDocResponse.body);
+    });
+});
