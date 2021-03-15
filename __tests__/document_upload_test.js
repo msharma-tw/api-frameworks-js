433399e193afbbd5ed58c2834e7528a9ea82a17a
msharma-tw <monika.sharma@thoughtworks.com>
2021-03-15 17:17:28 +0530

    update doc upload response | monika

--- __tests__/document_upload_test.js
+++ __tests__/document_upload_test.js
@@ -13,28 +13,9 @@ it("POST /document/upload - should UPLOAD THE DOCUMENT", function () {
     frisby
       .post("http://localhost:3003/document/upload", { body: form })
       // .inspectRequest()
-      .expect("status", 200)
+      .expect("status", 201)
       .then(function (response) {
         console.log(response.json);
       })
   );
-});
-
-it("test issue", function () {
-  const template = path.resolve("./zipFile.zip");
-  let content = fs.createReadStream(template);
-  const form = frisby.formData();
-
-  form.append("fileName", content);
-  form.append("fileType", ".zip");
-  form.append("fileName", "zipFile.zip");
-  form.append("fileType", "*.zip");
-
-  return frisby
-    .post("https://the-internet.herokuapp.com/upload", {
-      body: form,
-    })
-    .inspectHeaders()
-    .inspectBody()
-    .expect("status", 200);
 });
